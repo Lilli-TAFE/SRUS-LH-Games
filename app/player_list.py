3934d8102c8e8f8fbbe4ea1de1fc96f9ffa4a011
Lilli Hooper <20175438@tafe.wa.edu.au>
2026-08-10 16:02:30 +0800

    Started fleshing out Double Linked List functionality

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,18 @@
+from app.player import Player
+from app.player_node import PlayerNode
+
+
 class PlayerList:
     def __init__(self):
-        head = None
+        self.head = None
+        self.tail = None
+        self.is_empty = self.head is None and self.tail is None
+
+    def push_head(self, player: Player):
+        if self.is_empty:
+            self.head = PlayerNode(player)
+        else:
+            old_head = self.head
+            self.head = PlayerNode(player)
+            self.head.set_next(old_head)
+            old_head.set_prev(self.head)
