a2befdbc42c54af8db89ea30cff84e4c01f23b57
Lilli Hooper <20175438@tafe.wa.edu.au>
2026-08-17 16:07:43 +0800

    Made instance varianles private

--- app/player_list.py
+++ app/player_list.py
@@ -4,16 +4,16 @@ from app.player_node import PlayerNode
 
 class PlayerList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
-        self.is_empty = self.size == 0
+        self._head = None
+        self._tail = None
+        self._size = 0
+        self._is_empty = self.size == 0
 
     def push_head(self, player: Player):
-        if self.is_empty:
-            self.head = PlayerNode(player)
+        if self._is_empty:
+            self._head = PlayerNode(player)
         else:
-            old_head = self.head
-            self.head = PlayerNode(player)
-            self.head.set_next(old_head)
-            old_head.set_prev(self.head)
+            old_head = self._head
+            self._head = PlayerNode(player)
+            self._head.set_next(old_head)
+            old_head.set_prev(self._head)
